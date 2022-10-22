02d22a3d1e4469e08c59bd07900fda1087a58c97
Badshah  Yadav <96272309+BadshahYadav@users.noreply.github.com>
2022-10-22 15:33:50 +0530

    Documentation of file (#111)
    Co-authored-by: Badshah <badshahkumar1508@gmail.com>

---
+++ index.js
@@ -0,0 +1,32 @@
+// a little JS for the mobile nav button 
+
+var year = new Date().getFullYear();
+console.log(year);
+document.getElementById("year").innerHTML = year;
+
+if (document.getElementById('nav-mobile-btn')) {
+    document.getElementById('nav-mobile-btn').addEventListener('click', function () {
+        if (this.classList.contains('close')) {
+            document.getElementById('nav').classList.add('hidden');
+            this.classList.remove('close');
+        } else {
+            document.getElementById('nav').classList.remove('hidden');
+            this.classList.add('close');
+        }
+    });
+}
+//   ######################################
+
+// a little JS for the mobile nav button
+
+if (document.getElementById('nav-mobile-btn')) {
+    document.getElementById('nav-mobile-btn').addEventListener('click', function () {
+        if (this.classList.contains('close')) {
+            document.getElementById('nav').classList.add('hidden');
+            this.classList.remove('close');
+        } else {
+            document.getElementById('nav').classList.remove('hidden');
+            this.classList.add('close');
+        }
+    });
+}
