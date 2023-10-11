ad309f6a9faea21707cdd9a02209d9622ab943e5
Soniya Prasad <soniyagupta82270@gmail.com>
2023-10-11 17:22:51 +0530

    Translation of First Five chapters of this book in HINDI language fix issue #136 (changes-made) (#138)

---
+++ ebook/hi/ibis.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    /**
+     * The book title.
+     */
+    'title' => 'बैश-स्क्रिप्टिंग-का-परिचय',
+
+    /**
+     * The author name.
+     */
+    'author' => 'बॉबी इलिव',
+
+    /**
+     * The list of fonts to be used in the different themes.
+     */
+    'fonts' => [
+        //        'calibri' => 'Calibri-Regular.ttf',
+        //        'times' => 'times-regular.ttf',
+    ],
+
+    /**
+     * Page ranges to be used with the sample command.
+     */
+    'sample' => [
+        [1, 35]
+    ],
+
+    /**
+     * A notice printed at the final page of a generated sample.
+     */
+    'sample_notice' => 'यह बॉबी इलिव द्वारा लिखित "इंट्रोडक्शन टू बैश स्क्रिप्टिंग" का एक नमूना है। <br>
+    अधिक जानकारी के लिए, <a href="https://bobbyiliev.com/">यहाँ क्लिक करें</a>.',
+];
